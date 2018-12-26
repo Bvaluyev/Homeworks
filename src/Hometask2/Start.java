9ee82b6afce9a813b95b5210525d073de8c2298b
Bvaluyev <45964487+Bvaluyev@users.noreply.github.com>
2018-12-26 13:29:56 +0200

    Hometask2Start

---
+++ src/Hometask2/Start.java
@@ -0,0 +1,13 @@
+package Hometask2;
+
+class Start {
+   public static void main(String[] args){
+       System.out.println(Calculator.calcMethod1(100, 200, 300));
+       System.out.println(Calculator.calcMethod2(100, 200));
+       System.out.println(Calculator.calcMethod3(100, 200, 300));
+       System.out.println(Calculator.calcMethod4(100, 200, 300));
+    }
+
+}
+   // Реализовать вызов соответствующих методов из метода
+  //  void main(String[] args)
