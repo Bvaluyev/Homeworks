99d3a881740e4ac4328cb510b7a0fe13750913ac
Bvaluyev <45964487+Bvaluyev@users.noreply.github.com>
2019-01-14 21:54:18 +0200

    Homework 6: Факториал и Прогрессию ниасилил. Переделал DoWhile.

--- src/Homework6/DoWhile.java
+++ src/Homework6/DoWhile.java
@@ -1,20 +1,21 @@
 package Homework6;
 
 public class DoWhile {
-    public static void main(String[] args) {
-        DoWhile(10);
+        public static void main(String[] args) {
+        DoWhile();
     }
-
-    public static int DoWhile(int x) {
-        do {
-            if (x % 1 == 0) {
-                System.out.println(x);
-            }
-            x--;
-        } while (x >= 1);
-        return x;
+    public static int DoWhile(){
+        int i = 10;
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while (i>=1);
+        return i;
     }
 }
 
+
 //Организовать цикл do..while обратного отсчета от 10 до 1 в выводом на экран.
 //сделать как отдельный метод.
+
