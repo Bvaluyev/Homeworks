50dd8f31b36c42b7b8053f4fcc31d7cfd99893d4
Bvaluyev <45964487+Bvaluyev@users.noreply.github.com>
2019-01-18 14:01:18 +0200

    Выполнил. Но криво работает вывод в консоль. Подскажи плз )

---
+++ src/Homework8/EagleAndTails.java
@@ -0,0 +1,44 @@
+package Homework8;
+
+
+public class EagleAndTails {
+    public static void main(String[] args) {
+
+        EagleAndTails();
+    }
+
+    public static void EagleAndTails() {
+        int Eagle = 0;
+        int Tail = 0;
+
+        for (int i = 0; i < 2000; i++) {
+            int rnd = (int) (Math.random() * 2);
+            if (rnd == 1) {
+                Eagle = rnd +Eagle;
+
+            } else {
+                Tail = Tail + 1;
+            }
+            System.out.println("Орел выпал:" + " " + Eagle + " " + "раз.");
+            System.out.println("Решка выпала:" + " " + Tail + " " + "раз.");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+//    Смоделировать игру “Орел и Решка”.
+//        Бросить монетку 2000 раз и посчитать сколько раз выпал “орел”, а сколько раз “решка”.
+//        Подсказка: использовать генератор случайных чисел с генерацией чисел в диапазоне от 0 до 1. Если генератор выдает 1 - орёл, а 0 - решка.
+//
+//        Вывести результаты.
+//
+//
+
