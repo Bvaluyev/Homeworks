bd397eb78be9fb9655ecec2df2b74a59a05ce4fc
Bvaluyev <45964487+Bvaluyev@users.noreply.github.com>
2019-01-19 19:39:06 +0200

    Вынес SOUT за пределы цикла.

--- src/Homework8/EagleAndTails.java
+++ src/Homework8/EagleAndTails.java
@@ -22,8 +22,8 @@ public class EagleAndTails {
         }
         System.out.println("Орел выпал:" + " " + Eagle + " " + "раз.");
         System.out.println("Решка выпала:" + " " + Tail + " " + "раз.");
-
     }
+
 }
 
 
